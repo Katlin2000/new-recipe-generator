2dd705e67a42901fac0db2de0dc78c7daa497ab7
Katlin2000 <katlin.pala@gmail.com>
2024-11-24 18:54:22 +0000

    added ai ingration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Recipe will show here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "e5bt54135b02f36048218oa4bf5cb2d1";
+  let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
+  let context =
+    "You are an AI chef who likes to cook simple yet deliocous meals and has a knowledge of a wide range of recipes that are easy to prepare. Provide a recipe using the user instructions. Provide the recipe in the HTML format using </ br> between each line. Please do not display html before displaying the recipe. Please do not include any additinal comments. Display the recipe using Sevillana font";
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayRecipe);
+}
+
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
+//add axios cdn
