260d11384cc6a3084e4570b2ad64b50ce36b31d8
Katlin2000 <katlin.pala@gmail.com>
2024-11-25 21:05:26 +0000

    Improved user interface

--- src/index.js
+++ src/index.js
@@ -14,18 +14,16 @@ function generateRecipe(event) {
   let apiKey = "e5bt54135b02f36048218oa4bf5cb2d1";
   let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
   let context =
-    "You are an AI chef who likes to cook simple yet deliocous meals and has a knowledge of a wide range of recipes that are easy to prepare. Provide a recipe using the user instructions. Provide the recipe in the HTML format using </ br> between each line. Please do not display html before displaying the recipe. Please do not include any additinal comments. Display the recipe using Sevillana font";
+    "You are an AI chef who likes to cook simple yet deliocous meals and has a knowledge of a wide range of recipes that are easy to prepare. Provide a recipe using the user instructions. Provide the recipe in the HTML format using </ br> between each line. Please do not display html before displaying the recipe. Please do not include any additinal comments.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating recipe");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink">⏳ Generating a recipe with ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
-
-//add axios cdn
